1098dc5f881895278d19753faa856806da3ccef6
K. A <6701557+kseethreepio@users.noreply.github.com>
2023-01-07 16:15:56 -0600

    Refactoring square function to take side size as an argument. Also making a more involved rotating squares picture.

--- myturtle.py
+++ myturtle.py
@@ -3,11 +3,15 @@ from turtle import *
 shape('turtle')
 speed(0)
 
-def square():
+def square(size):
     for i in range(4):
-        forward(75)
+        forward(size)
         right(90)
 
-for i in range(60):
-    square()
+for i in range(72):
+    square(50)
     right(5)
+
+for i in range(36):
+    square(80)
+    right(10)
