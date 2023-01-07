a255c30a8526f2d4bd2572714a79f2305f0581b3
K. A <6701557+kseethreepio@users.noreply.github.com>
2023-01-07 16:01:15 -0600

    Refactored into function, made turtle turbo speed, and made a nifty thing of squares!

--- myturtle.py
+++ myturtle.py
@@ -1,7 +1,13 @@
 from turtle import *
 
 shape('turtle')
+speed(0)
 
-for i in range(4):
-    forward(75)
-    right(90)
+def square():
+    for i in range(4):
+        forward(75)
+        right(90)
+
+for i in range(60):
+    square()
+    right(5)
